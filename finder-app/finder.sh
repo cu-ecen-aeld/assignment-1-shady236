240ee1396a12fe813b6b1017a0b1cffb619c091b
Shady Mansour <ShadyMansour19@gmail.com>
2023-03-23 21:27:09 +0200

    finder-app done

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $# -ne 2 ];
+if [ $# -ne 2 ]
 then
 	echo "ERROR: Inavlid Number of Arguments."
 	echo "Total number of arguments should be 2."
@@ -25,5 +25,4 @@ else
 	echo "\t2)String to be searched in the specified directory path."
 	exit 1
 fi
-
 
