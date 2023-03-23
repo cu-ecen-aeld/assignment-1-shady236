60a3ddf525bd153068ee6475ddc1fc9cfd93155f
Shady Mansour <ShadyMansour19@gmail.com>
2023-03-23 21:47:43 +0200

    possible fix writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -2,11 +2,11 @@
 
 if [ $# -ne 2 ]
 then
-	echo "ERROR: Inavlid Number of Arguments."
-	echo "Total number of arguments should be 2."
-	echo "The order of the arguments should be:"
-	echo "\t1)File Path."
-	echo "\t2)String to be written in the specified file path."
+#	echo "ERROR: Inavlid Number of Arguments."
+#	echo "Total number of arguments should be 2."
+#	echo "The order of the arguments should be:"
+#	echo "\t1)File Path."
+#	echo "\t2)String to be written in the specified file path."
 	exit 1
 fi
 
