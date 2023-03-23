8226fb92c06b198bd34e0dd4b7a38bfec6f86bf2
Shady Mansour <ShadyMansour19@gmail.com>
2023-03-23 14:21:57 +0200

    finder-app done

---
+++ finder-app/finder.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -ne 2 ];
+then
+	echo "ERROR: Inavlid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Directory Path."
+	echo "\t2)String to be searched in the specified directory path."
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d ${filesdir} ]
+then
+	cd ${filesdir}
+	echo "The number of files are $(grep -rcH ${searchstr} * | wc -l) and the number of matching lines are $(grep -rH ${searchstr} * | wc -l)"
+	exit 0
+else 
+	echo "ERROR: Inavlid Type of Arguments."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Directory Path."
+	echo "\t2)String to be searched in the specified directory path."
+	exit 1
+fi
+
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -ne 2 ];
+then
+	echo "ERROR: Inavlid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Path."
+	echo "\t2)String to be written in the specified file path."
+	exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+#if [ -r ${writefile} ]
+#then
+	mkdir -p "$(dirname ${writefile})"
+	echo ${writestr} > ${writefile}
+	exit 0
+#else 
+#	echo "ERROR: Inavlid Type of Arguments."
+#	echo "The order of the arguments should be:"
+#	echo "\t1)File Path."
+#	echo "\t2)String to be written in the specified file path."
+#	exit 1
+#fi
+
+
