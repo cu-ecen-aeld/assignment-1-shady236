f97d117cf6222812bf3834cef505869537d48b08
Shady Mansour <ShadyMansour19@gmail.com>
2023-03-24 09:49:45 +0200

    possible fix writer.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
-#  filesdir = path to a directory on the filesystem
-#  searchstr = text string which will be searched within these files, referred to below as 
 
-# check number of arguments
-if [ $# -eq 2 ]
+if [ $# -ne 2 ]
 then
-  filesdir=$1
-  searchstr=$2
-else
-  echo command must take only 2 arguments!!
-  return 1 # Exits with return value 1 error
+	echo "ERROR: Inavlid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Directory Path."
+	echo "\t2)String to be searched in the specified directory path."
+	exit 1
 fi
 
-# check if first argument is path to directory 
-if [ -d $1 ]
+filesdir=$1
+searchstr=$2
+
+if [ -d ${filesdir} ]
 then
-  filesdir=$1
-else
-  echo argument 1: not a directory!!
-  return 1 # Exits with return value 1 error
+	cd ${filesdir}
+	echo "The number of files are $(grep -rcH ${searchstr} * | wc -l) and the number of matching lines are $(grep -rH ${searchstr} * | wc -l)"
+	exit 0
+else 
+	echo "ERROR: Inavlid Type of Arguments."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Directory Path."
+	echo "\t2)String to be searched in the specified directory path."
+	exit 1
 fi
 
-# find NO.files in the directory and all subdirectories
-x=$(find $1 -type f | wc -l)
-
-# number of matching lines found in respective files, where a matching line refers to a line which contains searchstr
-y=$(grep $2 $(find $1 -type f) | wc -l)
-
-# print result i terminal
-echo The number of files are $x and the number of matching lines are $y
