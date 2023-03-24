52f083ab03771e6c57ed05226dec343fb2f19d7c
Shady Mansour <ShadyMansour19@gmail.com>
2023-03-24 09:27:42 +0200

    possible fix writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -2,22 +2,33 @@
 
 if [ $# -ne 2 ]
 then
-#	echo "ERROR: Inavlid Number of Arguments."
-#	echo "Total number of arguments should be 2."
-#	echo "The order of the arguments should be:"
-#	echo "\t1)File Path."
-#	echo "\t2)String to be written in the specified file path."
+	echo "ERROR: Inavlid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "The order of the arguments should be:"
+	echo "\t1)File Path."
+	echo "\t2)String to be written in the specified file path."
 	exit 1
 fi
 
 writefile=$1
 writestr=$2
+writedir="$(dirname ${writefile})"
+
+mkdir -p $writedir 
+
+if [ -d $writedir ]
+then
+	echo ${writestr} > ${writefile}
+	exit 0
+else 
+	exit 1
+fi
 
 #if [ -r ${writefile} ]
 #then
-	mkdir -p "$(dirname ${writefile})"
-	echo ${writestr} > ${writefile}
-	exit 0
+#	mkdir -p "$(dirname ${writefile})"
+#	echo ${writestr} > ${writefile}
+#	exit 0
 #else 
 #	echo "ERROR: Inavlid Type of Arguments."
 #	echo "The order of the arguments should be:"
